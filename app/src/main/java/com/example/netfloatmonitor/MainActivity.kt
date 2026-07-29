46a95ebf67c004c3b7a3b5bb90cd14b862b680c2
Ws-Fly <785162600@qq.com>
2026-07-29 16:29:35 +0800

    Update MainActivity.kt

--- app/src/main/java/com/example/netfloatmonitor/MainActivity.kt
+++ app/src/main/java/com/example/netfloatmonitor/MainActivity.kt
@@ -5,6 +5,7 @@ import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
 import android.net.Uri
+import android.os.Build
 import android.os.Bundle
 import android.provider.Settings
 import android.widget.*
